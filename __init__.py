cbbe5103a2de7d5926f5b18f8e38edeaa1776a9e
Choon-Siang Lai <mycyberpet@yahoo.com>
2016-01-20 16:17:37 +0800

    cleaning up

--- __init__.py
+++ __init__.py
@@ -1,293 +1,136 @@
-from random import sample, randint, random
-from math import floor, pow, fabs, sqrt
+from random import sample
+from math import pow, sqrt
 from uuid import uuid4
-
-from numpy import argmin
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from itertools import chain
-import time
 from functools import reduce, partial
 from scipy.spatial.distance import euclidean
 
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
-def distance(*points):
-    return sqrt(reduce(lambda result, incoming: result + pow(incoming[0] - incoming[1], 2),
-                       zip(*points),
-                       0))
-
-# vec(OM) = (vec(OA) + vec(OB)) / 2
-def middle(points):
-    return tuple(map(lambda component: (component[0] + component[1]) / 2,
-                 zip(*points)))
-
-def pd_calculator(pnormal, middle):
-    d = reduce(lambda result, incoming: result + -(incoming[0] * incoming[1]),
-               zip(pnormal, middle),
-               0)
-
-    return partial(_pd_calculator, pnormal=pnormal, d=d)
-
-def _pd_calculator(point, pnormal, d):
-    return ((reduce(lambda result, incoming: result + incoming[0] * incoming[1], zip(pnormal, point), 0) + d) / sqrt(reduce(lambda result, incoming: result + pow(incoming, 2), pnormal, 0)))
-
-# AB = OA - OB
-def pnormal(points):
-    return tuple(map(lambda incoming: incoming[0] - incoming[1],
-                    zip(*points)))
-
-def split_points(points):
-    result = []
-
-    while(True):
-        result = sample(points, 2)
-
-        if reduce(lambda result, incoming: result or incoming[0] - incoming[1] != 0,
-                  zip(*result),
-                  False):
-            break
+def point_add(id, vector, result={}):
+    result[id] = vector
 
     return result
 
-def node_build(point_dict, points, leaf_max=5, node_id='ROOT'):
+def points_add(vectors, identifiers=None):
+    if identifiers:
+        return reduce(lambda result, i: point_add(identifiers[i], vectors[i], result),
+                      range(len(identifiers)),
+                      {})
+    else:
+        return reduce(lambda result, vector: point_add(uuid4(), vector, result),
+                      vectors,
+                      {})
+
+def node_build(points, point_ids, leaf_max=5, node_id='ROOT'):
     node, children = {}, []
 
-    if len(points) <= leaf_max:
+    if len(point_ids) <= leaf_max:
         node = {
             'type': 'leaf',
-            'count': len(points),
+            'count': len(point_ids),
             'id': node_id,
-            'children': points
+            'children': point_ids
         }
     else:
-        split = split_points([point for key, point in point_dict.items() if key in points])
-        pd_calc = pd_calculator(pnormal(split), middle(split))
-        negative, positive = [], []
-        children = [uuid4(), uuid4()]
+        split = split_points([point for key, point in points.items() if key in point_ids])
+        distance_calc = plane_point_distance_calculator(
+            plane_normal(*split),
+            plane_point(*split))
+        branches = {False: [], True: []}
+        child_ids = {False: uuid4(), True: uuid4()}
 
-        for point in points:
-            if pd_calc(point_dict[point]) > 0:
-                positive.append(point)
-            else:
-                negative.append(point)
+        for idx in point_ids:
+            branches[distance_calc(points[idx]) > 0].append(idx)
 
         node = {
             'type': 'branch',
-            'func': pd_calc,
-            'count': len(points),
+            'func': distance_calc,
+            'count': len(point_ids),
             'id': node_id,
-            'children': children
+            'children': [idx for _, idx in child_ids.items()]
         }
 
-        children = [partial(node_build, point_dict, negative, leaf_max, children[0]),
-                    partial(node_build, point_dict, positive, leaf_max, children[1])]
+        children.append(partial(node_build,
+                                points,
+                                branches[False],
+                                leaf_max,
+                                child_ids[False]))
+        children.append(partial(node_build,
+                                points,
+                                branches[True],
+                                leaf_max,
+                                child_ids[True]))
 
     return (node_id, node), children
 
-def tree(point_dict, leaf_max=5, n_jobs=1):
+def split_points(points):
+    result = sample(points, 2)
+
+    return result if reduce(lambda result, incoming: result or incoming[0] - incoming[1] != 0, zip(*result), False) else split_points(points)
+
+def plane_normal(*points):
+    return tuple(map(lambda incoming: incoming[0] - incoming[1],
+                    zip(*points)))
+
+def plane_point(*points):
+    return tuple(map(lambda component: (component[0] + component[1]) / 2,
+                 zip(*points)))
+
+def plane_point_distance_calculator(normal, point):
+    return partial(_ppd_calculator,
+                   normal=normal,
+                   d= reduce(lambda result, incoming: \
+                                result + -(incoming[0] * incoming[1]),
+                             zip(normal, point),
+                             0))
+
+def _ppd_calculator(point, normal, d):
+    return ((reduce(lambda result, incoming: result + incoming[0] * incoming[1], zip(normal, point), 0) + d) / sqrt(reduce(lambda result, incoming: result + pow(incoming, 2), normal, 0)))
+
+def tree_build(points, leaf_max=5):
     result = {}
 
-    tasks = [partial(node_build, point_dict, point_dict.keys(), leaf_max)]
-    while tasks:
-        tasks_next = []
-        for task in tasks:
-            node, sub_tasks = task()
+    builders = [partial(node_build, points, points.keys(), leaf_max)]
+    while builders:
+        builders_next = []
 
-            tasks_next = tasks_next + sub_tasks
-            result[node[0]] = node[1]
+        for builder in builders:
+            (node_id, node), builders_sub = builder()
 
-        tasks = tasks_next
+            builders_next = chain.from_iterable([builders_next, builders_sub])
+            result[node_id] = node
 
-    return result
-
-    #with ThreadPoolExecutor(max_workers=n_jobs) as pool:
-    #    tasks = [partial(node_build, points, leaf_max)]
-    #    while True:
-    #        jobs = []
-
-    #        for task in tasks:
-    #            jobs.append(pool.submit(task))
-
-    #        tasks = []
-
-    #        for job in jobs:
-    #            node, sub_tasks = job.result()
-
-    #            for st in sub_tasks:
-    #                tasks.append(st)
-
-    #            result[node[0]] = node[1]
-
-    #        if len(tasks) == 0:
-    #            break
-
-    #    return result
-
-def leaves_get(tree):
-    result = []
-
-    if tree['type'] == 'leaf':
-        result.append(tree)
-    else:
-        result = leaves_get(tree['children'][0]) + leaves_get(tree['children'][1])
+        builders = builders_next
 
     return result
 
-def leaves_nearest(point, tree, threshold, branch_id='ROOT', n_jobs=1):
+def query_neighbourhood(query, tree, threshold=0, start_id='ROOT'):
     result = []
 
-    branches = [branch_id]
+    branch_ids = [start_id]
 
-    while branches:
+    while branch_ids:
         branches_next = []
-        for branch_id in branches:
+
+        for branch_id in branch_ids:
             if tree[branch_id]['type'] == 'leaf':
                 result.append(tree[branch_id])
             else:
-                delta = tree[branch_id]['func'](point)
+                delta = tree[branch_id]['func'](query)
 
                 if threshold > 0 and -threshold < delta and delta < threshold:
-                    branches_next.append(tree[branch_id]['children'][0])
-                    branches_next.append(tree[branch_id]['children'][1])
-                elif delta > 0:
-                    branches_next.append(tree[branch_id]['children'][1])
+                    branches_next.append(tree[branch_id]['children'][False])
+                    branches_next.append(tree[branch_id]['children'][True])
                 else:
-                    branches_next.append(tree[branch_id]['children'][0])
+                    branches_next.append(tree[branch_id]['children'][delta > 0])
 
-        branches = branches_next
+            branch_ids = branches_next
 
     return result
 
-    #with ThreadPoolExecutor(max_workers=n_jobs) as pool:
-    #    tasks = ['ROOT']
-    #    while True:
-    #        jobs = []
-    #        for task in tasks:
-    #            if tree[task]['type'] == 'leaf':
-    #                result.append(tree[task])
-    #            else:
-    #                jobs.append((task, pool.submit(tree[task]['func'], point)))
+def search(query, points, neighbourhood):
+    candidates = tuple(chain.from_iterable([leaf['children'] for leaf in neighbourhood]))
+    distances = (euclidean(query, points[idx]) for idx in candidates)
 
-    #        tasks = []
-    #        for branch_id, job in jobs:
-    #            delta = job.result()
-
-    #            if delta > 0:
-    #                tasks.append(tree[branch_id]['children'][1])
-    #            elif delta > -threshold:
-    #                tasks.append(tree[branch_id]['children'][0])
-    #                tasks.append(tree[branch_id]['children'][1])
-    #            else:
-    #                tasks.append(tree[branch_id]['children'][0])
-
-    #        if len(tasks) == 0:
-    #            break
-
-    #    return result
-
-def distances_find(query, points):
-    return [euclidean(query, point) for point in points]
-
-def search_tree(query, nleaves, point_dict):
-    candidates = list(chain.from_iterable([leaf['children'] for leaf in nleaves]))
-    distances = distances_find(query, [point_dict[point] for point in candidates])
-    idx_min = argmin(distances)
-
-    return (distances[idx_min], candidates[idx_min])
-
-def search_brute(query, point_dict):
-    key, points = list(zip(*point_dict.items()))
-
-    distances = distances_find(query, points)
-    idx_min = argmin(distances)
-
-    return (distances[idx_min], key[idx_min])
-
-def dot(tree):
-    from graphviz import Digraph
-
-    dot = Digraph()
-    for branch_id, branch in tree.items():
-        if branch['type'] == 'branch':
-            dot.attr('node', shape='circle')
-        else:
-            dot.attr('node', shape='box')
-
-        dot.node(str(branch_id), str(branch['count']))
-
-    for branch_id, branch in tree.items():
-        if branch['type'] == 'branch':
-            dot.edge(str(branch_id), str(branch['children'][0]))
-            dot.edge(str(branch_id), str(branch['children'][1]))
-
-    return dot.source
-
-dim = 500
-points = {}
-#print('Generating Points')
-for i in range(1000):
-    points[uuid4()] = tuple([random() for __ in range(dim)])
-
-#print('Building Tree')
-_tree = tree(points, leaf_max=5, n_jobs=20)
-
-#print('total leaves: {}'.format(len([node for _, node in _tree.items() if node['type'] == 'leaf'])))
-#
-#query = tuple([random() for __ in range(dim)])
-#
-##print('Given Query {}'.format(query))
-##print('Cluster Answer')
-##t0 = time.clock()
-##nleaves = leaves_nearest(query, _tree, 0, n_jobs=2)
-##canswer = search_tree(query, nleaves)
-##print('Search took {} seconds'.format(time.clock() - t0))
-##print(canswer)
-#
-#print('Brutal Search Answer')
-#t0 = time.clock()
-#ganswer = search_brute(query, points)
-#print('Search took {} seconds'.format(time.clock() - t0))
-#print(ganswer)
-#
-#for threshold in range(0, int(1e7) + 1, 5):
-#    print('Cluster Answer for threshold {}'.format(threshold / 100))
-#    t0 = time.clock()
-#    nleaves = leaves_nearest(query, _tree, threshold / 100, n_jobs=20)
-#    canswer = search_tree(query, nleaves, points)
-#    print('Search took {} seconds'.format(time.clock() - t0))
-#    print('N-leaves: {}'.format(len(nleaves)))
-#    print(canswer)
-#
-#    if canswer[0] == ganswer[0]:
-#        break
-#
-######################################
-## Figure generation
-#
-##fig = plt.figure()
-##ax = fig.add_subplot(111)
-##
-##nleaves_uuid = [leaf['id'] for leaf in nleaves]
-##
-##color = [random() for _ in range(3)]
-##for leaf in [node for _, node in _tree.items() if node['type'] == 'leaf']:
-##    leaf_color = [random() for _ in range(3)] if leaf['id'] in nleaves_uuid else color
-##
-##    for point in leaf['children']:
-##        ax.plot([point[0]],
-##                [point[1]],
-##                #[point[2]],
-##                color=leaf_color,
-##                marker='x' if leaf['id'] in nleaves_uuid else '+',
-##                markersize=10 if point in [canswer[1], ganswer[1]] else 5)
-##
-##query_color = [random() for _ in range(3)]
-##ax.plot([query[0]],
-##        [query[1]],
-##        #[query[2]],
-##        color=query_color,
-##        marker='o')
-##
-##plt.show()
+    return sorted(zip(candidates, distances), key=lambda _: _[-1])
