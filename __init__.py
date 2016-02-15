e1220b3b5ae7a3949b5885d5ca75d78bce320173
Choon-Siang Lai <mycyberpet@yahoo.com>
2016-02-15 11:15:24 +0800

    allow export the forest as a dot file

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 from itertools import chain
 from functools import reduce, partial
 from operator import sub
+from graphviz import Digraph
 
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
@@ -226,3 +227,39 @@ def tree_build(points, leaf_max=5):
         builders = builders_next
 
     return root_id, result
+
+def node_get_dot(forest, node_id, is_root, dot):
+    children = []
+
+    if is_root:
+        dot.node(node_id, '')
+
+    if forest[node_id]['type'] == 'branch':
+        for child_id in forest[node_id]['children']:
+            if forest[child_id]['type'] == 'branch':
+                dot.node(child_id, '')
+            else:
+                dot.node(child_id, str(forest[child_id]['count']))
+
+            dot.edge(node_id, child_id)
+
+            children.append(partial(node_get_dot, forest, child_id, False, dot))
+
+    return children, dot
+
+def forest_get_dot(forest, count=1):
+    result = Digraph()
+
+    builders = [partial(node_get_dot, forest['forest'], root_id, True, result) for root_id in forest['roots']]
+
+    while builders:
+        builders_next = []
+
+        for builder in builders:
+            builders_sub, result = builder()
+
+            builders_next = chain(builders_next, builders_sub)
+
+        builders = builders_next
+
+    return result
